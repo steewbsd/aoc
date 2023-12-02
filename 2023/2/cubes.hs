f83e64ce2e1ea8c8c4f34fc07c3a2c3587235f45
Daniel Perez <steew0x8@protonmail.com>
2023-12-02 23:15:26 +0100

    [Day 2]: Format

--- 2023/2/cubes.hs
+++ 2023/2/cubes.hs
@@ -88,12 +88,19 @@ data Game = Game
 main :: IO ()
 main = do
   f <- readFile "input"
-  print $ foldr (\ (Game n _) acc -> acc + n) 0
-    -- $ filter (\(Game num (Cube nred Red : Cube ngreen Green : Cube nblue Blue : _)) -> nred < maxRed && nblue < maxBlue && ngreen < maxGreen)
-    $ filter (\(Game num cubes) -> not $ any (\(Cube n c) -> case c of
-                                                          Red -> n > maxRed
-                                                          Green -> n > maxGreen
-                                                          Blue -> n > maxBlue) cubes)
+  print
+    $ foldr (\(Game n _) acc -> acc + n) 0
+    $ filter
+      ( \(Game num cubes) ->
+          not $
+            any
+              ( \(Cube n c) -> case c of
+                  Red -> n > maxRed
+                  Green -> n > maxGreen
+                  Blue -> n > maxBlue
+              )
+              cubes
+      )
     $ map
       ( \l -> case parse lineParser "Error" l of
           Left _ -> Game 0 []
