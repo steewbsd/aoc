b3fab606e9b9224f181dbbae4c36741a8c9492c3
Daniel P. <daniel@steew.eu>
2024-12-07 14:11:51 +0100

    Finish day 2, part 1

--- 2024/2/1.lisp
+++ 2024/2/1.lisp
@@ -17,54 +17,45 @@
          table))
       )))
 
-(defun check-ascending (list firstp)
-  (when
-      (and
-       (not (null (car list)))
-       (not (null (cdr list))))
-    (let ((increment (abs (- (car list) (cadr list)))))
-      (cond
-        ((and
-          (< (car list) (cadr list))
-          (> increment 0) (< increment 4))
-         (check-ascending (cdr list) nil))
-        ('t (progn
-              (format
-               t
-               "ASC:Test '<' failed on: ~a ~a, dist: ~a ~%" (car list) (cadr list) increment)
-              nil))
-        )))
-  (when
-      (and (null (cdr list)) (not firstp))
-    (format t "Ascend test succeeded~%")
-    t))
+;;; Checks whether two numbers in a sequence fullfill the requirements of:
+;;; - Ascending, - Descending, - Step higher than 0, - Step lower than 4
+(defun check-sequence (a b)
+  (let* ((step (- b a))
+         (step-abs (abs step)))
+    (cond ((and (> step-abs 0) (< step-abs 4))
+           (cond ((plusp step) 'ascending)
+                 ((minusp step) 'descending)))
+          (t (progn
+               ;; (format t "Reached invalid range! ~a~%" step)
+               'invalid-step)))))
 
-(defun check-descending (list firstp)
-  (when
-      (and
-       (not (null (car list)))
-       (not (null (cdr list))))
-    (let ((decrement (abs (- (car list) (cadr list)))))
-      (cond
-        ((and
-          (> (car list) (cadr list))
-          (> decrement 0) (< decrement 4))
-         (check-descending (cdr list) nil))
-        ('t (progn
-              (format
-               t
-               "DSC:Test '<' failed on: ~a ~a, dist: ~a ~%" (car list) (cadr list) decrement)
-              nil))
-        )))
-  (when
-      (and (null (cdr list)) (not firstp))
-    (format t "Descend test succeeded~%")
-    t))
+;;; Applies check-sequence for a line
+(defun line-sequence (line sequences)
+  ;; Apply recursively
+  (if (cdr line)
+      (progn
+        ;(format t "~a~%" sequences)    
+        (line-sequencep 
+         (cdr line) 
+         (append sequences (list (check-sequence (car line) (cadr line))))))
+      sequences
+      ))
+
+;;; Check if all elements are ascending or descending
+(defun sequencep (line)
+  ;; (format t "~a~%" line)
+  (or 
+   (every (lambda (x) (equal x 'ascending)) line)
+   (every (lambda (x) (equal x 'descending)) line))
+  )
+
+(defun declare-safe (table)
+  (let ((applied-sequence (mapcar (lambda (x) (line-sequencep x ())) table)))
+    ;; (format t "~a~%" applied-sequence)
+    (mapcar #'sequencep applied-sequence)))
 
 (defvar table (read-to-list (open "./input") ()))
 (defvar parsed-table (mapcar (lambda (x) (mapcar #'parse-integer x)) table))
 
-(defun declare-safe (table)
-  (mapcar (lambda (x) (or (check-ascending x 't) (check-descending x 't))) table))
 
-(format t "~a~%" (declare-safe parsed-table))
+(format t "~a~%" (length (remove-if #'null (declare-safe parsed-table))))
